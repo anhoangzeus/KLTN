c5d3855092576de68ee47d63f715d94b5e3c9cc4
anhoangzeus <anhoangzeus@gmail.com>
2021-04-11 11:41:57 +0700

    check login gio hang va chinh header login

--- src/routers/TopTabNavigator/LoginTab.js
+++ src/routers/TopTabNavigator/LoginTab.js
@@ -1,49 +1,56 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import {View, Image, StyleSheet, SafeAreaView} from 'react-native';
 import LoginScreen from 'scenes/auth/login/login.container';
 import RegisterScreen from 'scenes/auth/register/register.container';
-import { HeaderBackButton } from '@react-navigation/stack';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import {HeaderBackButton} from '@react-navigation/stack';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import NavigationServices from 'utils/navigationServices';
 import SCENE_NAMES from 'constants/sceneName';
 
 const TabTop = createMaterialTopTabNavigator();
 
 const styles = StyleSheet.create({
-    containner: {
-        flex: 1,
-    },
-    image: {
-        width: '100%',
-        height: '15%',
-    },
-    headderButton: {
-        position: 'absolute',
-        color: 'red',
-    },
-    texthead: {
-        position: 'absolute',
-    },
+  containner: {
+    flex: 1,
+  },
+  containnersafe: {
+    flex: 1,
+    backgroundColor: '#2196f3',
+  },
+  image: {
+    width: '100%',
+    height: '15%',
+  },
+  headderButton: {
+    position: 'absolute',
+    color: 'red',
+  },
+  texthead: {
+    position: 'absolute',
+  },
 });
 
-const TopStackLogin = ({ props }) => {
-    return (
-        <View style={styles.containner}>
-            <Image
-                source={require('../../assets/images/shop2.png')}
-                style={styles.image}
-            />
-            <HeaderBackButton style={styles.texthead} onPress={() => NavigationServices.navigate(SCENE_NAMES.MAIN)} />
-            <TabTop.Navigator
-                tabBarOptions={{
-                    activeTintColor: 'blue',
-                }}
-            >
-                <TabTop.Screen name="Đăng nhập" component={LoginScreen}
-                />
-                <TabTop.Screen name="Đăng kí" component={RegisterScreen} />
-            </TabTop.Navigator></View>
-    );
+const TopStackLogin = ({props}) => {
+  return (
+    <SafeAreaView style={styles.containnersafe}>
+      <View style={styles.containner}>
+        <Image
+          source={require('../../assets/images/shop2.png')}
+          style={styles.image}
+        />
+        <HeaderBackButton
+          style={styles.texthead}
+          onPress={() => NavigationServices.navigate(SCENE_NAMES.MAIN)}
+        />
+        <TabTop.Navigator
+          tabBarOptions={{
+            activeTintColor: 'blue',
+          }}>
+          <TabTop.Screen name="Đăng nhập" component={LoginScreen} />
+          <TabTop.Screen name="Đăng kí" component={RegisterScreen} />
+        </TabTop.Navigator>
+      </View>
+    </SafeAreaView>
+  );
 };
 export default TopStackLogin;
-
